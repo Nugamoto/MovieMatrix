b2073ad91b19e3f0bea2b7b24ca0a1e47d0028f4
Lukas <21Nuga@pm.me>
2025-05-08 18:35:42 +0200

    feat: implement get_all_users method using session factory

--- datamanager/sqlite_data_manager.py
+++ datamanager/sqlite_data_manager.py
@@ -2,9 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from data_manager_interface import DataManagerInterface
+from datamanager.models import User
 
 
 class SQLiteDataManager(DataManagerInterface):
     def __init__(self, db_url: str):
-        self.engine = create_engine(self.db)
-        self.session = sessionmaker(bind=self.engine)
+        self.engine = create_engine(db_url)
+        self.Session = sessionmaker(bind=self.engine)
+
+    def get_all_users(self):
+        with self.Session() as session:
+            users = session.query(User).all()
+            return users
