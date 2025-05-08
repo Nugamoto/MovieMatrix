80057750d3a911b90631c593fb06f4c52e987502
Lukas <21Nuga@pm.me>
2025-05-08 19:01:16 +0200

    feat: implement add_user method with automatic ID refresh

--- datamanager/sqlite_data_manager.py
+++ datamanager/sqlite_data_manager.py
@@ -11,11 +11,22 @@ class SQLiteDataManager(DataManagerInterface):
         self.Session = sessionmaker(bind=self.engine)
 
     def get_all_users(self):
+        """Return all users from the database."""
         with self.Session() as session:
-            users = session.query(User).all()
-            return users
+            all_users = session.query(User).all()
+            return all_users
 
     def get_user_movies(self, user_id: int):
+        """Return all movies that belong to a specific user."""
         with self.Session() as session:
             movies = session.query(Movie).filter(Movie.user_id == user_id).all()
             return movies
+
+    def add_user(self, name: str):
+        """Add a new user to the database and return the user object."""
+        with self.Session() as session:
+            user = User(name=name)
+            session.add(user)
+            session.commit()
+            session.refresh(user)
+            return user
