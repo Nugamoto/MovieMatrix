e657f7c150404d56dad9210a67a0d693fbb87fe6
Lukas <21Nuga@pm.me>
2025-05-09 13:56:41 +0200

    feat: add /users route and template to display all registered users

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 
+from datamanager.sqlite_data_manager import SQLiteDataManager
+
 app = Flask(__name__)
+data_manager = SQLiteDataManager("sqlite:///moviematrix.sqlite")
 
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+
+@app.route('/users')
+def list_users():
+    users = data_manager.get_all_users()
+    return render_template("users.html", users=users)
 
 
 if __name__ == "__main__":
