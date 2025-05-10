0eaa11760d0c251890dea7e4b673635d900c3461
Lukas <21Nuga@pm.me>
2025-05-10 13:16:53 +0200

    test: fix delete user test by dynamically resolving user ID after creation

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,8 @@ import pytest
 # Add root directory to import path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+from datamanager.sqlite_data_manager import SQLiteDataManager
+
 from app import app
 
 
@@ -14,3 +16,8 @@ def client():
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
+
+
+@pytest.fixture
+def data_manager():
+    return SQLiteDataManager("sqlite:///moviematrix.sqlite")
